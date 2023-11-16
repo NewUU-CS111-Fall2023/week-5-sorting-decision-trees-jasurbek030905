917a8d8d5e392f8fbae8ab64c2950350f840e828
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2023-11-16 22:18:31 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,27 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void bubbleSort(vector<string>& titles) {
+    int n = titles.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (titles[j] > titles[j + 1]) {
+                swap(titles[j], titles[j + 1]);
+            }
+        }
+    }
+}
+int main() {
+    vector<string> bookTitles {"Catcher in the Rye", "Pride and Prejudice", "To Kill a Mockingbird",
+                                         "The Great Gatsby", "Moby Dick"};
+    for (const auto& title : bookTitles) {
+        cout << title << endl;
+    }
+    bubbleSort(bookTitles);
+    cout << "\nSorted list of book titles:\n";
+    for (const auto& title :bookTitles) {
+        cout << title << endl;
+    }
+    return 0;
+}
