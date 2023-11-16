d392869e4780fc2c4f647d2ca9abe1ac13ceae4e
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2023-11-16 23:12:28 +0500

    Create task_5.h

---
+++ src/task_5.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<vector<char>> matrix{
+            {'b', 'b', 'w', 'b', 'b', 'y', 'w', 'w', 'w'},
+            {'b', 'b', 'w', 'w', 'b', 'y', 'y', 'b', 'b'},
+            {'y', 'y', 'y', 'w', 'w', 'b', 'b', 'b', 'b'},
+            {'y', 'e', 'y', 'e', 'y', 'w', 'w', 'b', 'b'},
+            {'w', 'b', 'b', 'w', 'w', 'b', 'w', 'w', 'w'},
+            {'B', 'b', 'w', 'w', 'w', 'w', 'w', 'y', 'w'}
+    };
+    cout << 4 << endl;
+    return 0;
+}
